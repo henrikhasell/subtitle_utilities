62ea00849e641fdde28c4112ac4f14da384e82ee
Henrik Hasell <henrik@henrik.si>
2021-01-14 14:18:20 +0000

    Cleaned up code and updated logic.

--- proof_of_concept/__main__.py
+++ proof_of_concept/__main__.py
@@ -25,16 +25,38 @@ class Language:
         part2b: str,
         name: str
     ):
-        self.part1 = part1
-        self.part2t = part2t
-        self.part2b = part2b
-        self.name = name
+        self._part1 = part1
+        self._part2t = part2t
+        self._part2b = part2b
+        self._name = name
+
+    @property
+    def part1(self: object) -> str:
+        return self._part1
+
+    @property
+    def part2t(self: object) -> str:
+        return self._part2t
+
+    @property
+    def part2b(self: object) -> str:
+        return self._part2b
+
+    @property
+    def name(self: object) -> str:
+        return self._name
 
     def __str__(self: object):
         return self.name
 
     def __repr__(self: object):
         return f'<Language {str(self)}>'
+
+    def __hash__(self: object) -> int:
+        return hash(repr(self))
+
+    def __eq__(self: object, other: object) -> bool:
+        return type(self) is type(other) and hash(self) == hash(other)
 
     @staticmethod
     def from_string(string: str):
@@ -74,7 +96,7 @@ class Stream:
             self.is_valid_subtitle()
 
 
-def load_stream(stream_json: dict) -> List[Stream]:
+def load_stream(stream_json: dict) -> Stream:
         stream_tags = stream_json.get('tags')
 
         if stream_tags:
@@ -151,110 +173,6 @@ class ExternalSubtitle:
         )
 
 
-# BAZARR_PATH='/mnt/nfs/pi.croydon.vpn/bazarr'
-# 
-# CODEC_CHAR_MAP = {
-#     CodecType.Audio: 'a',
-#     CodecType.Video: 'v',
-#     CodecType.Subtitle: 's'
-# }
-# 
-# 
-# 
-# 
-# def get_stream_list(path: str) -> List[Stream]:
-#     command = [
-#         'ffprobe',
-#         '-print_format', 'json',
-#         '-show_streams',
-#         path
-#     ]
-# 
-#     result = subprocess.run(command, capture_output=True)
-#     result.check_returncode()
-# 
-#     return list(map(load_stream, json.loads(result.stdout)['streams']))
-# 
-# 
-# def is_valid_subtitle(stream: Stream):
-#     return stream.codec_type == CodecType.Subtitle and \
-#         stream.codec_name in {'dvd_subtitle', 'subrip'}
-#     
-# 
-# def is_valid_stream(stream: Stream):
-#     if stream.codec_type in {CodecType.Audio, CodecType.Video}:
-#         return True
-#     return is_valid_subtitle(stream)
-# 
-# 
-# def missing_subtitles(stream_list: List[Stream]) -> List[Language]:
-#     valid_subtitles = list(filter(is_valid_subtitle, stream_list))
-# 
-#     subtitle_status = {i: False for i in Language}
-# 
-#     for subtitle in valid_subtitles:
-#         for language in Language:
-#             if subtitle.language in COUNTRY_CODES[language]:
-#                 subtitle_status[language] = True
-#                 break
-# 
-#     return [i for i in Language if not subtitle_status[i]]
-# 
-# 
-# def find_subtitle_file(name: str, language: Language) -> Optional[str]:
-#     for country_code in COUNTRY_CODES[language]:
-#         file_name = f'{BAZARR_PATH}/{name}.{country_code}.srt'
-#         if os.path.isfile(file_name):
-#             return file_name
-# 
-# 
-# def find_subtitles(name: str, languages: List[Language]) -> List[Tuple[Language, str]]:
-#     result = []
-# 
-#     for language in languages:
-#         file_name = find_subtitle_file(name, language)
-# 
-#         if not file_name:
-#             continue
-# 
-#         result += [(language, file_name)]
-# 
-#     return result
-# 
-# 
-# def analyse_movie(path: str) -> None:
-#     stream_list = get_stream_list(path)
-# 
-#     valid_streams = list(filter(is_valid_stream, stream_list))
-#     valid_subtitles = list(filter(is_valid_subtitle, stream_list))
-# 
-#     required_subtitles = missing_subtitles(stream_list)
-#     found_subtitles = find_subtitles(Movie(path).name, required_subtitles)
-# 
-#     command = ['ffmpeg', '-i', path]
-# 
-#     for subtitle in found_subtitles:
-#         command += ['-i', subtitle[1]]
-# 
-#     for stream in valid_streams:
-#         command += ['-map', f'0:{stream.index}']
-# 
-#     for index in range(len(found_subtitles)):
-#         command += ['-map', f'{index + 1}:s']
-# 
-#     for item in enumerate(map(lambda i: i[0], found_subtitles)):
-#         offset, language = item
-#         index = len(valid_subtitles) + offset
-#         cc_string = COUNTRY_CODES[language][-1]
-#         command += [f'-metadata:s:s:{index}', f'language={cc_string}']
-# 
-#     command += ['-scodec', 'copy']
-#     command += ['output.mkv']
-# 
-#     result = subprocess.run(command)
-#     result.check_returncode()
-
-
 class Analyser:
     def find_movies(
         self: object,
@@ -281,7 +199,6 @@ class Analyser:
 
     def __init__(
         self: object,
-        wanted_languages: List[Language],
         movie_dir: str,
         subtitle_dir: str
     ):
@@ -300,46 +217,48 @@ class Analyser:
             self.subtitle_map[subtitle.name][language] = subtitle.path
 
         for movie in self.find_movies(movie_dir):
-            print(f'Movie name: {movie.name}')
-            missing_subtitles = movie.missing_subtitles(wanted_languages)
-            print(f'Missing subtiles: {list(map(lambda i: i.part1, missing_subtitles))}')
-            available_subtitles = [i for i in missing_subtitles if self.get_subtitle_path(movie.name, i)]
-            print(f'Available subtiles: {list(map(lambda i: i.part1, available_subtitles))}')
+            available_languages = list(map(Language.from_string,
+                self.subtitle_map.get(movie.name, [])))
 
-            if not missing_subtitles or not available_subtitles:
+            existing_languages = set(
+                [i.language for i in movie.find_subtitles()])
+
+            missing_languages = list(filter(
+                lambda i: i not in existing_languages, available_languages))
+
+            if not missing_languages:
                 continue
 
-            output_filename = f'{movie_dir}/{movie.name}/{movie.name} subtitles.mkv'
+            output_directory = '/'.join(movie.path.split('/')[0:-1])
+            output_path = f'{output_directory}/{movie.name} subtitles.mkv'
 
             command = ['ffmpeg', '-i', movie.path]
         
-            for subtitle in available_subtitles:
-                subtitle_path = self.get_subtitle_path(movie.name, subtitle)
+            for language in missing_languages:
+                subtitle_path = self.get_subtitle_path(movie.name, language)
                 command += ['-i', subtitle_path]
         
             for stream in filter(lambda i: i.is_valid_stream(), movie.streams):
                 command += ['-map', f'0:{stream.index}']
         
-            for index in range(len(available_subtitles)):
+            for index , _value in enumerate(missing_languages):
                 command += ['-map', f'{index + 1}:s']
         
-            for index, language in enumerate(available_subtitles):
+            for index, language in enumerate(missing_languages):
                 offset = len(movie.subtitles) + index
                 command += [f'-metadata:s:s:{offset}', f'language={language.part2b}']
         
             command += ['-scodec', 'copy']
-            command += ['test.mkv']
+            command += [output_path]
+            print(command)
+            continue
         
             result = subprocess.run(command)
             result.check_returncode()
 
-            exit(1)
-
-
 
 if __name__ == '__main__':
     analyser = Analyser(
-        list(map(Language.from_string, ['en', 'sl', 'ko', 'ja', 'zh'])),
-        '/mnt/nfs/pi.croydon.vpn/radarr',
-        '/mnt/nfs/pi.croydon.vpn/bazarr'
+        '/mnt/nas/pi.croydon.vpn/radarr',
+        '/mnt/nas/pi.croydon.vpn/bazarr'
     )
